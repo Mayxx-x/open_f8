3a8a7617ca586ec47b158cfabe25e30d732a1567
Alvaro <84032134+Mayxx-x@users.noreply.github.com>
2023-08-13 13:45:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 
-async function open_mic() {
-    let pass = '022216';
+function open_mic() {
+    let host_pass = '022216';
     let host_ip = '192.168.15.8';
     
     $('#mic').toggleClass('active');
 
     $('#status_output').html("Ativado!");
 
-    let response = await fetch('https://${host_ip}/9000/?pass=${host_pass}&action=mic&on=1');
+
+    if ($('#mic').hasClass('active') {
+        mic(0);
+    }
+    else {
+        mic(1);
+    }
+
+    async function mic(btn) {
+        if (btn == 1) {
+            await   fetch('https://${host_ip}:9000/?pass=${host  _pass}&cmd=mic&on=1');
+
+    }
+    elseif {
+         await fetch('https://${host_ip}:9000/?pass=${host_pass}&cmd=mic&on=0');
+
+    }
 
     console.log(window.location.href);
 }
